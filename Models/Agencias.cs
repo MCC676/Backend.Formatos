8c9bedaf4e157d495ab6721f0ec7cba6a03d6236
MCC676 <676teg@gmail.com>
2025-07-09 01:06:14 -0500

    feat: mapeo de tablas a entidades

---
+++ Models/Agencias.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendFormatos.Models
+{
+    public class Agencias
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? NombreAgencias { get; set; }
+
+        [Required]
+        public string? Direccion { get; set; }
+
+        [Required]
+        public string? Ruc { get; set; }
+
+        public bool Estado { get; set; }
+    }
+}
---
+++ Models/Exportadores.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendFormatos.Models
+{
+    public class Exportadores
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? NombreExportador { get; set; }
+
+        [Required]
+        public string? Direccion { get; set; }
+
+        [Required]
+        public string? Ruc { get; set; }
+
+        public bool Estado { get; set; }
+
+      
+    }
+}
